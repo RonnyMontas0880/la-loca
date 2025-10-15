1e83aef0dbc48ed4cd66ddf4493c354bee3d74f5
Ronny Montas <Ing.ronnymm2006@gmail.com>
2025-10-15 19:02:50 -0400

    ellocodejosias

---
+++ test_vscode.py
@@ -0,0 +1,7 @@
+# test_vscode.py
+
+def saludo(nombre):
+    return f"Hola, {nombre}. Visual Studio Code está funcionando correctamente."
+
+nombre_usuario = input("ronny: ")
+print(saludo(nombre_usuario))
